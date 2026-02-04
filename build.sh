0ba4c7c62901b3e91b507d366d9fcb7a34b3ab49
Haruki <haruki.isono861@gmail.com>
2026-02-04 15:06:09 +0900

    fix

---
+++ build.sh
@@ -0,0 +1,44 @@
+## set properties ####################
+CUR_DIR="$PWD"
+DST_DIR="$PWD/build"
+SRC_NAM="main"
+
+## create a build directory ##########
+echo "CUR_DIR=$CUR_DIR"
+echo "DST_DIR=$DST_DIR"
+if [ ! -d $DST_DIR ]; then
+	mkdir $DST_DIR
+	echo "$DST_DIR created"
+else
+	echo "$DST_DIR exists"
+fi
+
+## generate a pdf file ###############
+function gen_pdf () {
+	# build latex
+	platex $2.tex
+	bibtex $2.aux
+	platex $2.tex
+
+	# embed fonts
+	dvipdfmx -f $2.map $2.dvi
+	pdffonts $2.pdf
+
+	# compress PDF
+	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$2_min.pdf $2.pdf
+
+	# move pdf files to the destination directory
+	mv $2.pdf $1/$2.pdf
+	mv $2_min.pdf $1/$2_min.pdf
+
+	# remove intermediate files
+	rm $2.aux
+	rm $2.bbl
+	rm $2.blg
+	rm $2.dvi
+	rm $2.out
+	rm $2.log
+}
+
+## generate pdf files ###############
+gen_pdf $DST_DIR $SRC_NAM
